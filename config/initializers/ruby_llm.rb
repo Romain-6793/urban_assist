5630887b599d658652074625acb7fd1766efc6d6
Romain Claudot <romain_claudot@hotmail.com>
2026-09-25 10:40:30 +0200

    base url re-modified

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,6 @@
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.openai_api_base = "https://models.github.ai/inference"
+  config.openai_api_base = "https://models.github.ai/inference/"
   config.default_model = "openai/gpt-4o-mini"
   # ... see RubyLLM configuration guide for other models
 end
