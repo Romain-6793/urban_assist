40f9a9c3c8354d4123fcbc48d0c06607a5efebbc
Romain Claudot <romain_claudot@hotmail.com>
2026-09-25 09:11:54 +0200

    modified base url

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.openai_api_base = "https://models.inference.ai.azure.com"
+  config.openai_api_base = "https://models.github.ai/inference"
   # ... see RubyLLM configuration guide for other models
 end
